81df0deec2d6b9725350cbb777fd6e537efa5103
zohac <fenrir0680@gmail.com>
2018-05-12 16:27:31 +0200

    Creation of UserController and list method is terminated.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+
+class UserController extends FOSRestController
+{
+    /**
+     * @Rest\Get(
+     *      path="/users",
+     *      name="user_list"
+     * )
+     * 
+     * @Rest\View()
+     */
+    public function list(UserRepository $manager, UserInterface $user)
+    {
+        return $manager->findAllWhithAllEntities($user);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * 
  * @ORM\Table(name="user")
  * @UniqueEntity(fields="email", message="Email déjà utilisé")
+ * @UniqueEntity(fields="username", message="Pseudo déjà utilisé.")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
@@ -22,6 +23,11 @@ class User implements UserInterface
      * @ORM\Column(type="integer")
      */
     private $id;
+    
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $username;
 
     /**
      * @ORM\Column(type="string", length=100)
@@ -34,7 +40,7 @@ class User implements UserInterface
     private $lastName;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
@@ -169,5 +175,24 @@ class User implements UserInterface
         $this->customer = $customer;
 
         return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     */
+    public function eraseCredentials()
+    {
     }
 }
