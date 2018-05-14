90cc0d7b46e18343fa441b97955fc063450dc8c6
zohac <fenrir0680@gmail.com>
2018-05-14 20:16:21 +0200

    Creation of create method in UserController.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use FOS\RestBundle\Controller\Annotations as Rest;
+use Doctrine\Common\Persistence\ObjectManager;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 //use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
@@ -43,6 +46,38 @@ class UserController extends FOSRestController
      */
     public function detail(User $user)
     {
+        return $user;
+    }
+
+    /**
+     * @Rest\Post(
+     *      path="/api/users",
+     *      name="users_detail",
+     * )
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     *
+     * @Rest\View(StatusCode = 201)
+     *
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function create(
+        User $user,
+        UserInterface $originator = null,
+        UserPasswordEncoderInterface $encoder,
+        ObjectManager $entityManager
+    ) {
+        // Set the Customer
+        $user->setCustomer($originator->getCustomer());
+        // Encode the password
+        $password = $encoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+        // Set Role
+        $user->setRoles(['ROLE_USER']);
+
+        // Save the new user
+        $entityManager->persist($user);
+        $entityManager->flush();
+
         return $user;
     }
 }
