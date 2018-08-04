bf4b411eb923ed0ee788fc156aec203d356e988c
zohac <fenrir0680@gmail.com>
2018-08-04 18:43:26 +0200

    build(core): setting up the error management

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,45 +4,61 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
 class ExceptionListener
 {
+    /**
+     * @var Serializer
+     */
     private $serializer;
-    private $normalizers;
 
-    public function __construct(SerializerInterface $serializer)
+    public function __construct()
     {
-        $this->serializer = $serializer;
+        // Create new normalizer
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
+        // Add Circular reference handler
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        // Create new serializer
+        $this->serializer = new Serializer([$normalizer], [new JsonEncoder()]);
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         // You get the exception object from the received event
         $exception = $event->getException();
-        $message = sprintf(
-            'My Error says: %s with code: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
 
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent($result);
+        //var_dump($exception); die;
+        $messages = (method_exists($exception, 'getMessages')) ? $exception->getMessages() : $exception->getMessage();
+
+        $message[] = [
+            'code' => $exception->getStatusCode(),
+            'messages' => $messages,
+        ];
+
+        // Customize the response object to display the exception details
+        $response = new JsonResponse();
+        $response->setContent($this->serializer->serialize(['errors' => $message], 'json'));
 
         // HttpExceptionInterface is a special type of exception that
         // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        $satusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
 
-        
+        if ($exception instanceof HttpExceptionInterface) {
+            $satusCode = $exception->getStatusCode();
+        }
+        $response->setStatusCode($satusCode);
+        $response->headers->set('Content-Type', 'application/json');
+
         // sends the modified response object to the event
         $event->setResponse($response);
     }
--- src/Exception/ResourceValidationException.php
+++ src/Exception/ResourceValidationException.php
@@ -2,6 +2,55 @@
 
 namespace App\Exception;
 
+use Symfony\Component\Validator\ConstraintViolationList;
+
 class ResourceValidationException extends \Exception
 {
+    /**
+     * @var ConstraintViolationList
+     */
+    private $violations;
+
+    /**
+     * The default status code.
+     * 422: Unprocessable entity
+     *
+     * @var int
+     */
+    private $statusCode = 422;
+
+    /**
+     * Constructor.
+     *
+     * @param ConstraintViolationList $violations
+     */
+    public function __construct(ConstraintViolationList $violations)
+    {
+        $this->violations = $violations;
+    }
+
+    /**
+     * Get Messages.
+     *
+     * @return array
+     */
+    public function getMessages(): array
+    {
+        $messages = [];
+        foreach ($this->violations as $violation) {
+            $messages[] = sprintf('Field %s: %s', $violation->getPropertyPath(), $violation->getMessage());
+        }
+
+        return $messages;
+    }
+
+    /**
+     * Get the value of statusCode.
+     *
+     * @return int
+     */
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
 }
