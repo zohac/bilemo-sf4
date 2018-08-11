4b848427a407b365bb037be2760c489e5eef0ac5
zohac <fenrir0680@gmail.com>
2018-08-11 15:54:38 +0200

    docs(core): setting up the documentattion with NelmioApiDoc

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,19 +4,50 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Swagger\Annotations as SWG;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
+/**
+ * Controller managing the products
+ */
 class ProductController extends FOSRestController
 {
     /**
+     * Get the list of all product.
+     * 
      * @Rest\Get(
-     *      path="/products",
+     *      path="/api/products",
      *      name="product_list"
      * )
      *
-     * @Rest\View()
+     * @Rest\View(StatusCode = 200)
+     * 
+     * @SWG\Get(
+     *     description="Get the list of products.",
+     *     tags = {"Product"},
+     *     @SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *     ),
+     *     @SWG\Response(
+     *         response="401",
+     *         description="Unauthorized: JWT Token not found / Expired JWT Token / Invalid JWT Token",
+     *     ),
+     *     @SWG\Response(
+     *          response=405,
+     *          description="Method Not Allowed"
+     *     ),
+     *     @SWG\Parameter(
+     *          name="Authorization",
+     *          required= true,
+     *          in="header",
+     *          type="string",
+     *          description="Bearer Token",
+     *     )
+     * )
      */
     public function list(ProductRepository $manager)
     {
@@ -25,13 +56,49 @@ class ProductController extends FOSRestController
 
     /**
      * @Rest\Get(
-     *      path="/products/{id}",
+     *      path="/api/products/{id}",
      *      name="product_detail",
      *      requirements = {"id"="\d+"}
      * )
      * @Entity("product", expr="repository.findOneWhithAllEntities(id)")
      *
-     * @Rest\View()
+     * @Rest\View(StatusCode = 200)
+     * 
+     * @SWG\Get(
+     *     description="Get one product.",
+     *     tags = {"Product"},
+     *     @SWG\Response(
+     *          response=200,
+     *          @Model(type=Product::class),
+     *          description="successful operation"
+     *     ),
+     *     @SWG\Response(
+     *         response="401",
+     *         description="Unauthorized: JWT Token not found / Expired JWT Token / Invalid JWT Token",
+     *     ),
+     *     @SWG\Response(
+     *          response=405,
+     *          description="Method Not Allowed"
+     *     ),
+     *     @SWG\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *     ),
+     *     @SWG\Parameter(
+     *          name="id",
+     *          required= true,
+     *          in="path",
+     *          type="integer",
+     *          description="The product unique identifier.",
+     *     ),
+     *     @SWG\Parameter(
+     *          name="Authorization",
+     *          required= true,
+     *          in="header",
+     *          type="string",
+     *          description="Bearer Token",
+     *     )
+     * )
      */
     public function detail(Product $product)
     {
